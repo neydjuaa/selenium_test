79b3f6b59c66592bc6956d2c17630e73abedf39e
muharrem <marslandevops@gmail.com>
2024-04-22 16:39:29 +0200

    ahah

--- tests/test.js
+++ tests/test.js
@@ -1,43 +1,45 @@
-//const { defaultInstance } = require("chromedriver");
 const { By, Key, Builder } = require("selenium-webdriver");
 require("chromedriver");
+ 
 
 async function test_case() {
-    let driver = await new Builder().forBrowser("chrome").build();
+  let driver = await new Builder().forBrowser("chrome").build();
 
-    await driver.get("https://www.bing.com");
-    try {
-        // Initialize WebDriver session
-        driver = await new Builder().forBrowser("chrome").build();
 
-        await driver.findElement(By.id("sb_form_q")).sendKeys("Hello, World!", Key.RETURN);
-        // Navigate to google
 
-        console.log("Opened Google.");
+  await driver.get("https://www.inami.fgov.be/fr");
+  try {
+    // Initialize the WebDriver session
+   // driver = await new Builder().forBrowser("chrome").build();
 
-        // Wait for some time before quitting
-        await driver.sleep(10000);
-        // Find the search input element
-        const searchInput = await driver.findElement(By.id("sb_form_q"));
+  await driver.findElement(By.name("q")).sendKeys("Hello, World!", Key.RETURN);
+    // Navigate to Google
 
-        // Quit the WebDriver session
-        await driver.quit();
-        // Enter text into the search input
-        await searchInput.sendKeys("Hello, World!", Key.RETURN);
+    console.log("Opened Google.");
 
-        console.log("Text entered into the search input.");
+  // Wait for some time before quitting
+  await driver.sleep(10000);
+    // Find the search input element
+    const searchInput = await driver.findElement(By.name("q"));
 
-        // Wait for some time before quitting
-        await driver.sleep(10000);
-    } catch (error) {
-        console.error("An error occured:", error);
-    } finally {
-        // Close the WebDriver session
-        if (driver) {
-            await driver.quit();
-            console.log("WebDriver session closed.");
-        }
+  // Quit the WebDriver session
+  await driver.quit();
+    // Enter text into the search input
+    await searchInput.sendKeys("Hello, World!", Key.RETURN);
+
+    console.log("Text entered into the search input.");
+
+    // Wait for some time before quitting
+    await driver.sleep(10000);
+  } catch (error) {
+    console.error("An error occurred:", error);
+  } finally {
+    // Close the WebDriver session
+    if (driver) {
+      await driver.quit();
+      console.log("WebDriver session closed.");
     }
+  }
 }
 
 test_case();
