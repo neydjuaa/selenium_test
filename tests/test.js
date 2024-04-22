b0fa5d99b6a0106db23f1de956ac97567ef85869
muharrem <marslandevops@gmail.com>
2024-04-22 14:15:10 +0200

    haha

--- tests/test.js
+++ tests/test.js
@@ -10,7 +10,7 @@ async function test_case() {
         // Initialize WebDriver session
         driver = await new Builder().forBrowser("chrome").build();
 
-        await driver.findElement(By.name("q")).sendKeys("Hello, World!", Key.RETURN);
+        await driver.findElement(By.id("sb_form_q")).sendKeys("Hello, World!", Key.RETURN);
         // Navigate to google
 
         console.log("Opened Google.");
@@ -18,7 +18,7 @@ async function test_case() {
         // Wait for some time before quitting
         await driver.sleep(10000);
         // Find the search input element
-        const searchInput = await driver.findElement(By.name("q"));
+        const searchInput = await driver.findElement(By.id("sb_form_q"));
 
         // Quit the WebDriver session
         await driver.quit();
