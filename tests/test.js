7be90e2bd431704122921ac527ef27fc686c128e
muharrem <marslandevops@gmail.com>
2024-04-22 13:16:47 +0200

    commit

--- tests/test.js
+++ tests/test.js
@@ -1,27 +1,21 @@
-//const { defaultInstance } = require("chromedriver");
 const { By, Key, Builder } = require("selenium-webdriver");
 require("chromedriver");
 
 async function test_case() {
-    let driver = await new Builder().forBrowser("chrome").build();
+    let driver;
 
-    await driver.get("https://www.bing.com");
     try {
         // Initialize WebDriver session
         driver = await new Builder().forBrowser("chrome").build();
 
-        await driver.findElement(By.name("q")).sendKeys("Hello, World!", Key.RETURN);
-        // Navigate to google
+        await driver.get("https://www.bing.com");
+        console.log("Opened Bing.");
 
-        console.log("Opened Google.");
+        // Wait for some time before searching
+        await driver.sleep(3000);
 
-        // Wait for some time before quitting
-        await driver.sleep(10000);
         // Find the search input element
         const searchInput = await driver.findElement(By.name("q"));
-
-        // Quit the WebDriver session
-        await driver.quit();
         // Enter text into the search input
         await searchInput.sendKeys("Hello, World!", Key.RETURN);
 
@@ -30,7 +24,7 @@ async function test_case() {
         // Wait for some time before quitting
         await driver.sleep(10000);
     } catch (error) {
-        console.error("An error occured:", error);
+        console.error("An error occurred:", error);
     } finally {
         // Close the WebDriver session
         if (driver) {
